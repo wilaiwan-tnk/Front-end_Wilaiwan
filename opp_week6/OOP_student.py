31032a20278beafaf21a96b056b1d8e13086b6aa
wilaiwan-tnk <67319010035@tnk.ac.th>
2024-11-22 16:31:32 +0700

    Update OOP_student.py

--- opp_week6/OOP_student.py
+++ opp_week6/OOP_student.py
@@ -1,25 +1,31 @@
-import random
+import random 
 
-class student:
+class studert:
     def __init__(self,ชื่อนามสกุล,ชื่อเล่น):
-        self.name = ชื่อนามสกุล
-        self.nikename = ชื่อเล่น 
-        self.score = random.randint(1,10)
+       self.name = ชื่อนามสกุล
+       self.nikename = ชื่อเล่น
+       self.score = random.randint(1,10)
+       self.editscore = random.randint(1,10)
 
     def scores(self):
         if self.score >= 5:
             print(f"ชื่อ-นามสกุล : {self.name} , ชื่อเล่น : {self.nikename}, คะแนน : {self.score} : คุณผ่าน")
         else :
             print(f"ชื่อ-นามสกุล : {self.name} , ชื่อเล่น : {self.nikename}, คะแนน : {self.score} : คุณตก")
+            print("--------------------สอบแก้---------------------------")
+        if self.editscore >= 5:
+            print(f"ชื่อ-นามสกุล : {self.name} , ชื่อเล่น : {self.nikename}, คะแนน : {self.editscore } : คุณผ่าน")
+        else :
+            print(f"ชื่อ-นามสกุล : {self.name} , ชื่อเล่น : {self.nikename}, คะแนน : {self.editscore } : คุณตก")
 
-student1 = student("หนึ่งธิดา อินทรชัย","สตางค์")
-student2 = student("ลินลาวดี ไกลถิ่น","เอลลี่")
-student3 = student("ศศิวมล แซ่ด่าน","แบม")
-student4 = student("ธันย์ชนก ไชยชนะ","เกด")
-student5 = student("นันทิชา ว่องย่อง","ไอซ์")    
+studert1 = studert("ธันย์ชนก ไชยชนะ","เกด")
+studert2 = studert("ลินลาวดี ไกลถิ่น","เฮลลี่")
+studert3 = studert("ศศิวมล แซ่ด่าน","แบม")
+studert4 = studert("วิไลวรรณ พลเดช","แมวเป้า")
+studert5 = studert("หนี่งธิดา อินทรชัย","ตัง")
 
-student1.scores()
-student2.scores()
-student3.scores()
-student4.scores()
-student5.scores()
+studert1.scores()
+studert2.scores()
+studert3.scores()
+studert4.scores()
+studert5.scores()
